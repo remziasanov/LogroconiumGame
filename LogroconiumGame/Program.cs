d536cf454a4863b9e332838d428f92b0925c676c
Remzi Asanov <rasanov@logrocon.com>
2025-03-23 12:33:41 +0300

    какркас игры добавление

---
+++ LogroconiumGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LogroconiumGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
